ecc10d96830f4b9c37bc23d55dd4a14ac01bbb83
LuongDzai <vuxuanluong12345@gmail.com>
2020-02-02 22:51:54 +0700

    Intro to Hooks

---
+++ hookapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return (
+        <div className="song-list">
+            <ul>
+                <li>this good</li>
+                <li>memory</li>
+            </ul>
+        </div>
+    );
+}
+
+export default SongList;
