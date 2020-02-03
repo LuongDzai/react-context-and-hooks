ed191eaebe03f720bb7a5ca3ba9f592d4785559c
LuongDzai <vuxuanluong12345@gmail.com>
2020-02-03 09:43:17 +0700

    Reading list app p3

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
 import BookList from './components/BookList';
+import NewBookForm from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
-        <Navbar/>
-        <BookList/>
+        <Navbar />
+        <BookList />
+        <NewBookForm />
       </BookContextProvider>
     </div>
   );
